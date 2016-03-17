507797c7e893989330ce98d225c1e4245fb2154c
Hiroyoshi Kurohara <kurohara@yk.rim.or.jp>
2016-03-17 21:05:02 +0900

    built as framework

---
+++ GTMHttpFetcher/GTMHttpFetcher.h
@@ -0,0 +1,19 @@
+//
+//  GTMHttpFetcher.h
+//  GTMHttpFetcher
+//
+//  Created by 黒原浩善 on 2016/03/17.
+//  Copyright © 2016年 Microgadget,inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GTMHttpFetcher.
+FOUNDATION_EXPORT double GTMHttpFetcherVersionNumber;
+
+//! Project version string for GTMHttpFetcher.
+FOUNDATION_EXPORT const unsigned char GTMHttpFetcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GTMHttpFetcher/PublicHeader.h>
+
+
